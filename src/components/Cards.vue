09be756d32ab92916363cbabe464459857db8503
จารุวิทย์ สิงห์โสม <jaruwit178@gmail.com>
2023-05-13 03:06:51 +0700

    o

---
+++ src/components/Cards.vue
@@ -0,0 +1,36 @@
+<template>
+     <div class="container main">
+        <div class="container">
+            <div class="card mt-4 " id="card-item" style="width: 18rem;">
+
+                <slot name="pictur">
+
+                </slot>
+                <div class="card-body">
+                    <slot name="names">
+
+                    </slot>
+                    <slot name="type">
+
+                    </slot>
+                    <slot name="content">
+
+                    </slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'card-item',
+  }
+
+
+</script>
+
+<style scoped>
+#card-item img{
+    width: 10rem;
+}
+</style>
--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -1,35 +1,41 @@
 <template>
-    <div class="container main">
-        <div class="container">
-            <div class="card mt-4 " id="card-item" style="width: 18rem;">
-                <img src="/Befor Login/camera Page/pic/camera/20221104_032921_sony-a7r-v-1.jpg" alt="Mirrorless-camera">
+    <Cards>
+        <template v-slot:pictur>
+            <img src="/Befor Login/camera Page/pic/camera/20221104_032921_sony-a7r-v-1.jpg" alt="Mirrorless-camera">
+        </template>
+            <template v-slot:names>
+                <p class="card-text fs-5">Canon G5X </p>
+            </template>
 
-                <div class="card-body">
-                    <p class="card-text fs-5">Canon G5X </p>
-                    <p class=" card-title">Type: Complex</p>
-                    <ul class="fw-light fs-6">
-                        <li>20.2MP 1" Stacked CMOS Sensor</li>
-                        <li>DIGIC 8 Image Processor</li>
-                        <li>5x Optical Zoom f/1.8-2.8 Lens</li>
-                        <li>24-120mm (35mm Equivalent)</li>
-                    </ul>
-                </div>
-            </div>
-    </div>
-    </div>
+            <template v-slot:type>
+                <p class="card-title">Type: Complex</p>
+            </template>
+
+            <template v-slot:content>
+                <ul class="fw-light fs-6">
+                    <li>20.2MP 1" Stacked CMOS Sensor</li>
+                    <li>DIGIC 8 Image Processor</li>
+                    <li>5x Optical Zoom f/1.8-2.8 Lens</li>
+                    <li>24-120mm (35mm Equivalent)</li>
+                </ul>
+            </template>
+
+
+    </Cards>
 </template>
 
 <script>
+import Cards from '../components/Cards.vue';
 export default {
-  name: 'Camera-page',
-  components: {
-  }
+    name: 'Camera-page',
+    components: {
+        Cards
+    }
 }
 </script>
 <style scoped>
-#card-item img{
-    
+#card-item img {
+
     width: 10rem;
 }
-
 </style>

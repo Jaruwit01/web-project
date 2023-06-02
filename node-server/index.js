1123e6ec1f58394c30d0980847085a94bd10e4a8
jaruwit01 <jaruwit178@gmail.com>
2023-06-02 12:06:41 +0700

    r

--- node-server/index.js
+++ node-server/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static('public'))
+
 
 app.post('/api/create', (request, response) => {
   let name = request.body.name || '';
@@ -49,3 +52,9 @@ app.post('/api/create', (request, response) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+mongoose.connect('mongodb://localhost:27017/express-demo', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.log(err));
